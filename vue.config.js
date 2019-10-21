702bb28ce292152e474233191fcd350fa5cf30ee
L <376693576@qq.com>
2019-10-21 11:07:11 +0800

    update 0.5.6

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: "/work/vue-ohyeah-scroll",
+  publicPath: process.env.NODE_ENV === "development" ? "/" : "/work/vue-ohyeah-scroll",
   // plugins: [
   //   [
   //     "component",
